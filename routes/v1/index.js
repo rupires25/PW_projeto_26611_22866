6f300a514a1b9de2f90cc8bf7a3d4ab9f68d6022
JoaoMonteiro00 <160851600+JoaoMonteiro00@users.noreply.github.com>
2024-06-18 04:10:00 +0100

    Delete routes directory

--- routes/v1/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const clientRouter = require('./clients');
-
-router.use('/clients', clientRouter);
-
-module.exports = router;
--- routes/v2/auth.js
+++
@@ -1,9 +0,0 @@
-const authRouter = require('express').Router();
-const controller = require('../../controllers/v2/auth');
-
-authRouter.post('/signin', controller.signin);
-authRouter.post('/signup', controller.signup);
-authRouter.post('/letoken', controller.readToken);
-
-
-module.exports = authRouter;
